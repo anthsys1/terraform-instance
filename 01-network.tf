2cfa7b79f806d13523a7ddc2b87d6f5b1d3b8319
anthsys1 <a.fouque@ecole-ipssi.net>
2022-06-03 17:39:23 +0200

    commit

--- 01-network.tf
+++ 01-network.tf
@@ -2,42 +2,42 @@ resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "main-vpc-groupe3"
+    Name = "main_vpc_groupe3"
   }
 }
 
-resource "aws_subnet" "SUBNET-PUBLIC" {
+resource "aws_subnet" "SUBNET_PUBLIC" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.0.0/24"
 
   tags = {
-    Name = "SUBNET-PUBLIC"
+    Name = "SUBNET_PUBLIC"
   }
 }
 
-resource "aws_subnet" "SUBNET-PRIVATE-A" {
+resource "aws_subnet" "SUBNET_PRIVATE-A" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.1.0/24"
 
   tags = {
-    Name = "SUBNET-PRIVATE-A"
+    Name = "SUBNET_PRIVATE_A"
   }
 }
 
-resource "aws_subnet" "SUBNET-PRIVATE-B" {
+resource "aws_subnet" "SUBNET_PRIVATE-B" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.2.0/24"
 
   tags = {
-    Name = "SUBNET-PRIVATE-B"
+    Name = "SUBNET_PRIVATE_B"
   }
 }
 
-resource "aws_subnet" "SUBNET-PRIVATE-C" {
+resource "aws_subnet" "SUBNET_PRIVATE-C" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.3.0/24"
 
   tags = {
-    Name = "SUBNET-PRIVATE-C"
+    Name = "SUBNET_PRIVATE_C"
   }
 }
