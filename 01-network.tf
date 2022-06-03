a0e4047548d230ab5f58b9a31bd2ebdbdc63ce47
anthsys1 <a.fouque@ecole-ipssi.net>
2022-06-03 17:41:03 +0200

    commit

--- 01-network.tf
+++ 01-network.tf
@@ -15,7 +15,7 @@ resource "aws_subnet" "SUBNET_PUBLIC" {
   }
 }
 
-resource "aws_subnet" "SUBNET_PRIVATE-A" {
+resource "aws_subnet" "SUBNET_PRIVATE_A" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.1.0/24"
 
@@ -24,7 +24,7 @@ resource "aws_subnet" "SUBNET_PRIVATE-A" {
   }
 }
 
-resource "aws_subnet" "SUBNET_PRIVATE-B" {
+resource "aws_subnet" "SUBNET_PRIVATE_B" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.2.0/24"
 
@@ -33,7 +33,7 @@ resource "aws_subnet" "SUBNET_PRIVATE-B" {
   }
 }
 
-resource "aws_subnet" "SUBNET_PRIVATE-C" {
+resource "aws_subnet" "SUBNET_PRIVATE_C" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.3.0/24"
 
