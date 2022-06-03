1dbd1902f59732cf2bbda93a0a799a6e000f881d
anthsys1 <a.fouque@ecole-ipssi.net>
2022-06-03 13:09:57 +0200

    commit

---
+++ 01-network.tf
@@ -0,0 +1,47 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc-groupe3"
+  }
+}
+
+resource "aws_subnet" "SUBNET-PUBLIC" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "eu-west-3"
+
+  tags = {
+    Name = "SUBNET-PUBLIC"
+  }
+}
+
+resource "aws_subnet" "SUBNET-PRIVATE-A" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-west-3"
+
+  tags = {
+    Name = "SUBNET-PRIVATE-A"
+  }
+}
+
+resource "aws_subnet" "SUBNET-PRIVATE-B" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-west-3"
+
+  tags = {
+    Name = "SUBNET-PRIVATE-B"
+  }
+}
+
+resource "aws_subnet" "SUBNET-PRIVATE-C" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "eu-west-3"
+
+  tags = {
+    Name = "SUBNET-PRIVATE-C"
+  }
+}
